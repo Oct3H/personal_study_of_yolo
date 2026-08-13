9045945941f74ef767047354cf839d66aa451bfc
WtanabeShin <steinner19331109@163.com>
2026-08-13 22:16:03 +0800

    Aug13: fix 1 bug

--- Main_.py
+++ Main_.py
@@ -6,22 +6,6 @@ from event import check_event_pic,check_event_video
 from processor import video_process, VideosInDir
 from visualizer import show_box
 
-'''-------------------------------------------------------------'''
-
-
-'''
-#功能组合执行(已废弃⚠️)
-#single pic/series of pics AND single frame ALL CAN use it to analyze animals and return bark
-def analyze_and_return_bark(A_Results):
-    for pic in A_Results:
-        for cls_number in pic.boxes.cls:#cls里的序号是「n.」，属于float；而names里的dict头是int，因此需要int化
-            item_name = pic.names[int(cls_number)]
-            player.play_sound(item_name)
-'''
-
-
-'''-------------------------------------------------------------'''
-
 
 
 def pic_execute(pic_path):
@@ -34,6 +18,7 @@ def pic_execute(pic_path):
 
 
 def video_execute(video_path):
+    detector.ResetTrack()
     video_generator = video_process(video_path)
     frame_id = 0#暂时没啥用。。。
     #every frame：
@@ -49,7 +34,7 @@ def video_execute(video_path):
             if check_event_video(name,id):
                 player.play_sound_video(name)
 
-#下一步解决当下最大的问题：视频事件逻辑/time先后顺序/叫声重叠等如何解决？能不能给一个「引导方案」？
+
 
 #文件夹里多个视频循环，每一个新视频都要重置一下track，防止污染
 def videos_folder_execute(folder_path):
@@ -59,12 +44,43 @@ def videos_folder_execute(folder_path):
         video_execute(video_path)
 
 
-
-
+'''----------------------------main---------------------------------'''
 
 #main:
 video_execute(yolo_source_video)
 pic_execute(yolo_source_pic)
 
-#两个bug：1.不显示可视化框  2.同一个动物重复触发bark（需要利用编号，同一个出现一次只叫一下）✅
-#疑问：video文件夹输入多个视频可以吗🤔
+
+
+
+
+
+
+
+
+#Question & Bug & Next Step
+#✅下一步解决当下最大的问题：视频事件逻辑/time先后顺序/叫声重叠等如何解决？能不能给一个「引导方案」？
+#✅两个bug：1.不显示可视化框  2.同一个动物重复触发bark（需要利用编号，同一个出现一次只叫一下）✅
+#✅疑问：video文件夹输入多个视频可以吗🤔
+
+
+
+
+
+
+
+'''-------------------------------------------------------------'''
+
+
+'''
+#功能组合执行(早已废弃⚠️)
+#single pic/series of pics AND single frame ALL CAN use it to analyze animals and return bark
+def analyze_and_return_bark(A_Results):
+    for pic in A_Results:
+        for cls_number in pic.boxes.cls:#cls里的序号是「n.」，属于float；而names里的dict头是int，因此需要int化
+            item_name = pic.names[int(cls_number)]
+            player.play_sound(item_name)
+'''
+
+
+'''-------------------------------------------------------------'''
--- detector.py
+++ detector.py
@@ -1,22 +1,6 @@
 from ultralytics import YOLO
 from config import yolo_model
 
-
-
-#error example：
-#                                       要想办法避免多个视频连续播放导致（如果一直是同一个track的话）track污染。。。
-#                                       老版本：如果在detector里确实没问题，但问题是main！ 这个函数是靠返回值重置模型的，但是main里面“接不住”
-#                                      或者说，接住了，但是return到main里，在这个文件里没有意义，因此应该用「global+无返回值函数」才行
-'''                                         def ResetTrack():
-                                            VideoModel_ = YOLO(yolo_model)
-                                            return VideoModel_
-                                            
-                                            
-                                            
-                                            VideoModel_ = ResetTrack()
-'''
-
-
 '''--------------重置器-----------------'''
 def ResetTrack():
     global VideoModel_
@@ -26,9 +10,7 @@ def ResetTrack():
 
 '''model name in this .py is Model_'''
 PicModel_ = YOLO(yolo_model)
-#VideoModel_ = YOLO(yolo_model)
-#VideoModel_ = ResetTrack()
-#要想办法避免track污染。。。
+ResetTrack()#要想办法避免track污染。。。
 
 
 
@@ -45,3 +27,23 @@ def DetectFrame(frame):
     results = VideoModel_.track(frame,persist=True)#persist=True: 标识track任务持续存在，不要每帧都重置id
     return results
 
+
+
+
+
+
+
+
+
+# error example：
+#                                       要想办法避免多个视频连续播放导致（如果一直是同一个track的话）track污染。。。
+#                                       老版本：如果在detector里确实没问题，但问题是main！ 这个函数是靠返回值重置模型的，但是main里面“接不住”
+#                                      或者说，接住了，但是return到main里，在这个文件里没有意义，因此应该用「global+无返回值函数」才行
+'''                                         def ResetTrack():
+                                            VideoModel_ = YOLO(yolo_model)
+                                            return VideoModel_
+
+
+
+                                            VideoModel_ = ResetTrack()
+'''
